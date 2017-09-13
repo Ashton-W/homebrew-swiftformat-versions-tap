0b3ecdbb2f3f291395fcb1b7c62ba8177170f49a
Ashton Williams <Ashton-W@users.noreply.github.com>
2017-09-13 12:02:16 +1000

    Add 0.29.5

---
+++ swiftformat@0.29.5.rb
@@ -0,0 +1,34 @@
+class SwiftformatAT0295 < Formula
+  desc "Formatting tool for reformatting Swift code"
+  homepage "https://github.com/nicklockwood/SwiftFormat"
+  url "https://github.com/nicklockwood/SwiftFormat/archive/0.29.5.tar.gz"
+  sha256 "f8878d9e9e4bf703c424270be6f9cf1215e768e820c3187db95999042eb61e89"
+
+  head "https://github.com/nicklockwood/SwiftFormat.git", :shallow => false
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "43b08b8bec3b04fb892293df91af3dca73eeff5ae48eafaaf8bcc8b39e1c6248" => :sierra
+    sha256 "e05834c5ddbbf1935d9fcfe42026494293adf6ecb1232b600a5c5e20fea46e29" => :el_capitan
+  end
+
+  depends_on :xcode => ["8.0", :build]
+
+  def install
+    xcodebuild "-project",
+        "SwiftFormat.xcodeproj",
+        "-scheme", "SwiftFormat (Command Line Tool)",
+        "CODE_SIGN_IDENTITY=",
+        "SYMROOT=build", "OBJROOT=build"
+    bin.install "build/Release/swiftformat"
+  end
+
+  test do
+    (testpath/"potato.swift").write <<-EOS.undent
+      struct Potato {
+        let baked: Bool
+      }
+    EOS
+    system "#{bin}/swiftformat", "#{testpath}/potato.swift"
+  end
+end
